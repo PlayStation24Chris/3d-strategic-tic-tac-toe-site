8c91285610fdf70ec93567cd95b2c406dd95eaeb
unknown <PS24CKowalewski@efcts.us>
2023-02-02 08:10:33 -0500

    remove resize script

--- resize.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("resize", changeSize);
-
-function changeSize()
-{
-    document.getElementById("titleText").style.fontSize = ((screen.width + 400) / 80) + "px";
-    console.log(((screen.width + 400) / 80) + "px");
-}
